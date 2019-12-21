344390882c8e6f3a8f613d2841781c666bc8247a
wxk <wxk@local.com>
2019-12-21 11:24:11 +0800

    第三次提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -3,5 +3,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("第一次提交");
         System.out.println("第二次提交");
+        System.out.println("第四次提交");
     }
 }
