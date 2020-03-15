3be4401f11956ca5f058249e84a21862d9d22604
wxk <wxk@local.com>
2020-03-15 18:08:42 +0800

    清除所有提交

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,8 +1,6 @@
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("第一次提交");
-        System.out.println("test1_第二次提交");
-        System.out.println("test1_第三次提交");
+        System.out.printf("清楚所有提交");
     }
 }
